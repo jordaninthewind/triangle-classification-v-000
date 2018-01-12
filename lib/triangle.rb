cf3de8b097f287ee10a9ff51d2d5033a7fa09e06
Jordan Kline <jordan.kline@gmail.com>
2018-01-12 00:40:29 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,12 +5,11 @@ attr_accessor :sides
     @side1 = side1
     @side2 = side2
     @side3 = side3
-    @sides = [@side1, @side2, @side3]
+    @sides = [@side1, @side2, @side3].sort
   end
 
   def kind
     sides = @sides.uniq
-
     if sides.length == 1 && @sides.all? { |e| e > 0  }
       return :equilateral
     elsif sides.length == 2 && @sides.all? { |e| e > 0  }
