f73631cb1e00b19bc30313cd49a22cdf2c1580a8
Jordan Kline <jordan.kline@gmail.com>
2018-01-12 00:08:31 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,10 +5,11 @@ attr_accessor :side1, :side2, :side3
     @side1 = side1
     @side2 = side2
     @side3 = side3
+    @sides = [@side1, @side2, @side3]
   end
 
   def kind
-    if @side1 == @side2 && @side2 == @side3
+    if 
       return :equilateral
     # elsif
     #   return :isosceles
