77df490de9a2d5591b1dd0bfd42bd9ba5216694c
Jordan Kline <jordan.kline@gmail.com>
2018-01-12 00:43:44 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,9 +11,10 @@ attr_accessor :sides
   def kind
     sides = @sides.uniq
     valid = @sides[0] + @sides[1] < @sides[2]
+    binding.pry
     if sides.length == 1 && @sides.all? { |e| e > 0  }
       return :equilateral
-    elsif sides.length == 2 && @sides.all? { |e| e > 0  } 
+    elsif sides.length == 2 && @sides.all? { |e| e > 0  }
       return :isosceles
     elsif sides.length == 3 && @sides.all? { |e| e > 0  }
       return :scalene
